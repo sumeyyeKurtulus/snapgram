ba6536eba410f81f1b97861546897e9c3400ad4b
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-17 19:22:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+import React from "react";
+
+export const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReacDOM from "react-dom/client";
+import App from "./App";
+
+ReacDOM.createRoot(document.getElementById("root")!).render(<App />);
