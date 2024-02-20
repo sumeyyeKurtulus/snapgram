bfe3babd415ffa80f88948cbd70f30ecbd1c20c9
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-20 19:47:48 +0300

    appwriteConfig is initialized 🧚🏻

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,15 @@
+import { Account, Avatars, Client, Databases, Storage } from "appwrite";
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  url: import.meta.env.VITE_APPWRITE_URL,
+};
+
+export const client = new Client();
+client.setProject("65d0d74e4771257d91ef"); //remove this before PUSH
+client.setEndpoint(appwriteConfig.url);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
