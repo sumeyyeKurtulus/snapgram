e9b8fa086237fa1a748e96612159c43105f7d178
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-21 17:02:58 +0300

    AuthProvider is added to main hook ✍🏻

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,15 @@ import React from "react";
 import ReacDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
+import { AuthProvider } from "./context/AuthContext";
 
-ReacDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReacDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
+);
