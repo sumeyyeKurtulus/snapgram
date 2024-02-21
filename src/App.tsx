8f13002ab99ec88567c5a9b9f1aec93aa0f09629
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-21 17:02:40 +0300

    Toaster component is added 🐼

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,24 @@ import { Home } from "./_root/pages";
 import SignUpForm from "./_auth/forms/SignUpForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
 
 export const App = () => {
   return (
     <main className="flex h-screen">
-      <Router>
-        {/* Wrap your Routes component with BrowserRouter */}
-        <Routes>
-          {/* Public Routes */}
-          <Route element={<AuthLayout />}>
-            <Route path="/sign-in" element={<SignInForm />}></Route>
-            <Route path="/sign-up" element={<SignUpForm />}></Route>
-          </Route>
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />}></Route>
+          <Route path="/sign-up" element={<SignUpForm />}></Route>
+        </Route>
 
-          {/* Private Routes */}
-          <Route element={<RootLayout />}>
-            <Route index element={<Home />}></Route>
-          </Route>
-        </Routes>
-      </Router>
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+      <Toaster />
     </main>
   );
 };
