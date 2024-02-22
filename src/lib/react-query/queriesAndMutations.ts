97ebac5bd4364529ada8458ac0ecc21b11e86879
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-22 18:34:02 +0300

    signOutAccountMutation is implemented 🐼

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,10 +1,9 @@
+import { useMutation } from "@tanstack/react-query";
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
 import { INewUser } from "@/types";
 
 export const useCreateUserAccountMutation = () => {
@@ -17,5 +16,11 @@ export const useSignInAccountMutation = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
       signInAccount(user),
+  });
+};
+
+export const useSignOutAccountMutation = () => {
+  return useMutation({
+    mutationFn: () => signOutAccount(),
   });
 };
